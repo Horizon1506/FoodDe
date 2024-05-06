360258bcb78e30f6a203f658c0e73b24a0815da8
NguyenDongHung <15062003h@gmail.com>
2024-05-06 11:22:45 +0700

    chore: Update map functionality in DeliveryScreen

--- client/screens/DeliveryScreen.js
+++ client/screens/DeliveryScreen.js
@@ -4,27 +4,57 @@ import { useNavigation } from '@react-navigation/native';
 import { featured } from '../constants';
 import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../slices/restaurantSlice';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const DeliveryScreen = () => {
 
-    const restaurant= useSelector(selectRestaurant);
+    const restaurant= featured.restaurants[0];
     const navigation = useNavigation();
     return (
-        <View style={styles.container}>
-            <MapView style={styles.map} />
+        <View className='flex-1'>
+            <MapView
+                initialRegion={{
+                    latitude: restaurant.lat,
+                    longitude: restaurant.lng,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+                
+                className='flex-1'
+                mapType='standard'
+            >
+                <Marker
+                    coordinate={{
+                        latitude: restaurant.lat,
+                        longitude: restaurant.lng,
+                    }}
+                    title={restaurant.name}
+                    description={restaurant.description}
+                       
+                />
+            </MapView>
+            <View className="rounded-t-3xl -mt-12 bg-white relative">
+                <View className='flex-row justify-between px-5 pt-10'>
+                    <View>
+                        <Text className="text-lg text-gray-700 font-semibold">
+                            Cao Minh Thiện
+                        </Text>
+                        <Text className='text-3xl  text-gray-700 font-extrabold'>
+                            10-20 phút
+                        </Text>
+                        <Text className="mt-2 text-gray-700 font-semibold">
+                            Đơn hàng đang giao đến bạn !!
+                        </Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    map: {
-      width: '100%',
-      height: '100%',
-    },
+    
+    
   });
 
 export default DeliveryScreen;
