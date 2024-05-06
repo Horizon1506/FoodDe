99c2a4bd4f961fdb95bf206adf4ad29c6b66e9f2
NguyenDongHung <15062003h@gmail.com>
2024-05-06 14:30:53 +0700

    chore: Update DeliveryScreen UI and add delivery car image

--- client/screens/DeliveryScreen.js
+++ client/screens/DeliveryScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, View ,Text} from 'react-native';
+import { StyleSheet, View ,Text, Image, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { featured } from '../constants';
 import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../slices/restaurantSlice';
 import MapView, { Marker } from 'react-native-maps';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 const DeliveryScreen = () => {
 
@@ -37,7 +39,7 @@ const DeliveryScreen = () => {
                 <View className='flex-row justify-between px-5 pt-10'>
                     <View>
                         <Text className="text-lg text-gray-700 font-semibold">
-                            Cao Minh Thiện
+                            Đang giao hàng
                         </Text>
                         <Text className='text-3xl  text-gray-700 font-extrabold'>
                             10-20 phút
@@ -45,6 +47,26 @@ const DeliveryScreen = () => {
                         <Text className="mt-2 text-gray-700 font-semibold">
                             Đơn hàng đang giao đến bạn !!
                         </Text>
+                    </View>
+                    <Image className="w-24 h-24 " source={require('../assets/images/deliveryCar.gif')}/>
+                </View>
+                <View
+                    style={{backgroundColor: 'orange'}}
+                    className="p-2 flex-row justify-between items-center rounded-full my-5 mx-2 "
+                >
+                    <View className="p-1 rounded-full"
+                        style={{backgroundColor:'rgba(255,255,255,0.4)'}}>
+                        <Image className="h-16 w-16  rounded-full"/>
+                    </View>
+                    <View className="flex-1 ml-3">
+                        <Text className="text-lg font-bold text-white">
+                            Cao Minh Thiện
+                        </Text>
+                    </View>
+                    <View className="flex-row items-center space-x-3 mr-3">
+                        <TouchableOpacity className="bg-white p-2 rounded-full">
+                            <MaterialIcons name="call" size={24} color="green" />
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
