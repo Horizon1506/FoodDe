b69762c9b119fde5e3785b1ac6113137d542f999
huyphanisme <huyphan20003@gmail.com>
2024-05-07 15:00:29 +0700

    css đăng kí và đăng nhập

--- client/screens/RegisterSrceen.js
+++ client/screens/RegisterSrceen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button,SafeAreaView,Image } from 'react-native';
+import { View, Text, TextInput, Button,SafeAreaView,Image,TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const RegisterScreen = () => {
@@ -19,28 +19,40 @@ const RegisterScreen = () => {
     <View>
       <Image style={{width:200,height:200,alignSelf: 'center',marginBottom: 20}} 
       source={require('../assets/images/signup.jpg')}  />
-      <Text style={{ fontSize: 20, textAlign: 'center' }}>Đăng ký</Text>
-      <TextInput style={{borderWidth:1, borderColor:'black', borderRadius:5, padding:5,marginTop:30}}
+      <Text style={{ fontSize: 30, textAlign: 'center',marginBottom:30,fontWeight:'bold' }}>Đăng ký</Text>
+      <TextInput style={{backgroundColor:'white',color:'orange', marginBottom: 10,padding: 10, borderRadius: 50,
+       fontSize: 20,textAlign:'center',}}
         placeholder="Username"
         value={username}
         onChangeText={setUsername}
       />
-      <TextInput style={{ borderWidth:1, borderColor:'black', borderRadius:5, padding:5,marginTop:10}}
+      <TextInput style={{backgroundColor:'white',color:'orange', marginBottom: 10,padding: 10, borderRadius: 50,
+       fontSize: 20,textAlign:'center',}}
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      <TextInput style={{borderWidth:1, borderColor:'black', borderRadius:5, padding:5,marginTop:10,marginBottom:30}}
+      <TextInput style={{backgroundColor:'white',color:'orange', marginBottom: 10,padding: 10, borderRadius: 50,
+       fontSize: 20,textAlign:'center',}}
         placeholder="Confirm Password"
         value={confirmPassword}
         onChangeText={setConfirmPassword}
         secureTextEntry
       />
-      <Button title="Đăng ký" onPress={handleRegister} />
-      <View style={{ borderBottomColor: 'black', borderBottomWidth: 1, margin: 10, marginTop: 30 }} />
-      <Text style={{ textAlign: 'center', marginTop: 15,marginBottom: 10 }}>Bạn đã có tài khoản</Text>
-      <Button title="Đăng nhập" onPress={(handleLogin) => { /* Navigate to login screen */ }} />
+     <TouchableOpacity style={{marginBottom: 10,padding: 10, borderRadius: 50,backgroundColor:'orange' }} 
+       onPress={handleLogin}>
+       <Text style={{textAlign:'center',color:'white',fontSize:20,fontWeight:'bold'}}> Đăng kí </Text>
+      </TouchableOpacity>   
+     
+      <View style={{ borderBottomColor: 'orange', borderBottomWidth: 1, margin: 10,marginTop:30 }} />
+      
+      <Text style={{textAlign:'center',marginTop:30,marginBottom:10}}>Bạn đã có tài khoản</Text>
+      
+      <TouchableOpacity style={{marginBottom: 10,padding: 10, borderRadius: 50,backgroundColor:'white', }} 
+       onPress={handleRegister}>
+       <Text style={{textAlign:'center',color:'orange',fontSize:20,fontWeight:'bold'}}> Đăng nhập </Text>
+      </TouchableOpacity>   
     </View>
     </SafeAreaView>
   );
